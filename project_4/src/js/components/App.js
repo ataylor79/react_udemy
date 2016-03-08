6ec570c6f61e073272a4b61b3ee5345d6a4070b0
Alec Taylor <alec.taylor79@gmail.com>
2016-03-08 23:24:29 +0000

    event dispatchers - searching and receiving data

--- project_4/src/js/components/App.js
+++ project_4/src/js/components/App.js
@@ -1,14 +1,33 @@
 var React = require('react');
 var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
+var SearchForm = require('./SearchForm.js');
+
+function getAppState() {
+  return {
+    movies: AppStore.getMovieResults()
+  };
+}
 
 var App = React.createClass({
+  getInitialState: function () {
+    return getAppState();
+  },
+  componentDidMount: function () {
+    AppStore.addChangeListener(this._onChange);
+  },
+  componentWillUnMount: function () {
+    AppStore.removeChangeListener(this._onChange);
+  },
   render: function () {
     return(
       <div>
-        My App
+        <SearchForm />
       </div>
     )
+  },
+  _onChange: function () {
+    this.setState(getAppState());
   }
 });
 
